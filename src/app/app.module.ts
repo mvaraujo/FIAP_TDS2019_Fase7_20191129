14df8757e304c48bf9e57b43cb59ee9797bb97f6
Marcus Vinicius Pompeu <mvaraujo@vanzolini-ead.org.br>
2019-11-03 17:54:39 -0300

    Chamada ao Google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { Camera } from '@ionic-native/camera/ngx';
+import { HttpClient, HttpHandler, HttpClientModule } from '@angular/common/http';
+import { Http, HttpModule, ConnectionBackend, RequestOptions } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,12 +16,15 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    Camera
+    Camera,
+    // tslint:disable-next-line: deprecation
+    HttpModule, Http, RequestOptions,
+    HttpClient
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Http, Headers, RequestOptions, HttpModule } from '@angular/http';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { AnnotateImageResponse, EntityAnnotation } from '@datafire/google_vision';
+import 'rxjs/add/operator/finally';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +13,27 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 })
 export class HomePage {
 
+  imagem: string;
+  loading: any;
+
   constructor(
     private loadingCtrl: LoadingController,
-    private camera: Camera
-  ) {}
+    private camera: Camera,
+    // tslint:disable-next-line: deprecation
+    private http: HttpModule,
+    private httpClient: HttpClient
+  ) { }
+
+  async presentLoading() {
+    this.loading = await this.loadingCtrl.create({
+      message: 'Processando'
+    });
+    await this.loading.present();
+  }
+
+  async dismissLoading() {
+    this.loading.dismiss();
+  }
 
   tirarFoto() {
     const options: CameraOptions = {
@@ -22,6 +43,72 @@ export class HomePage {
       mediaType: this.camera.MediaType.PICTURE
     };
 
-    this.camera.getPicture(options);
+    this.camera.getPicture(options)
+      .then(imagemDados => {
+        this.imagem = 'data:image/jpeg;base64,' + imagemDados;
+        return imagemDados;
+      })
+      .then(imagem => {
+        this.presentLoading();
+
+        return this.requisicaoParaAPI(imagem);
+      });
+  }
+
+  requisicaoParaAPI(imagemBase64) {
+    const apiKey = 'AIzaSyBLNjgYqCgkbLFNo5vsr-2m9vb7Yp6zz8g';
+    const url = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
+    const request = {
+      requests: [
+        {
+          image: {
+            content: imagemBase64
+          },
+          features: [
+            {
+              type: 'LABEL_DETECTION',
+              maxResults: 50
+            },
+            {
+              type: 'DOCUMENT_TEXT_DETECTION'
+            }
+          ]
+        }
+      ]
+    }
+
+    // tslint:disable-next-line: deprecation
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+
+    this.httpClient
+      .post(url, request)
+      .finally(
+        () => { this.dismissLoading(); }
+      )
+      .subscribe(
+        (result: AnnotateImageResponse) => {
+          let hasStreetSign = false;
+
+          for (const labelAnnotation of result.responses[0].labelAnnotations) {
+            if (labelAnnotation.description === 'Street sign') {
+              hasStreetSign = true;
+              break;
+            }
+          }
+
+          if (!hasStreetSign) {
+            alert(
+              'Isso não parece uma placa de rua!\n' +
+              'Por favor, tente de novo'
+            );
+          } else {
+            alert(
+              'A rua foi identificada\n' +
+              'Iremos mostrá-la no mapa!'
+            );
+          }
+        },
+        (error: HttpErrorResponse) => { alert(error.error.error.message); }
+      );
   }
 }
