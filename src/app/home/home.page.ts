8fa670122390f396c0804d392756d5017a9966bf
Marcus Vinicius Pompeu <mvsabino@gmail.com>
2019-11-09 23:03:07 -0300

    ADD Dummy caso cordova não esteja presente ADD Outros botões FIX Reconhece plata com outros parâmetros: 'Street sign', 'Traffic sign', 'Metropolitan area'

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,6 +5,7 @@ import { Http, Headers, RequestOptions, HttpModule } from '@angular/http';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { AnnotateImageResponse, EntityAnnotation } from '@datafire/google_vision';
 import 'rxjs/add/operator/finally';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -14,6 +15,8 @@ import 'rxjs/add/operator/finally';
 export class HomePage {
 
   imagem: string;
+  hasStreetSign: boolean;
+  address: string;
   loading: any;
 
   constructor(
@@ -21,6 +24,7 @@ export class HomePage {
     private camera: Camera,
     // tslint:disable-next-line: deprecation
     private http: HttpModule,
+    private router: Router,
     private httpClient: HttpClient
   ) { }
 
@@ -45,25 +49,40 @@ export class HomePage {
 
     this.camera.getPicture(options)
       .then(imagemDados => {
-        this.imagem = 'data:image/jpeg;base64,' + imagemDados;
-        return imagemDados;
+        this.imagem = imagemDados;
+
+        return {
+          content: this.imagem
+        };
       })
-      .then(imagem => {
+      .catch(error => {
+        if (error === 'cordova_not_available') {
+          // Dummy when cordova not available
+          this.imagem = 'https://saopauloesuasruas.files.wordpress.com/2015/05/placa-rua-s-bento.jpg';
+
+          return {
+            source: {
+              imageUri: this.imagem
+            }
+          };
+        } else {
+          throw error;
+        }
+      })
+      .then(imageRequest => {
         this.presentLoading();
 
-        return this.requisicaoParaAPI(imagem);
+        return this.requisicaoParaAPI(imageRequest);
       });
   }
 
-  requisicaoParaAPI(imagemBase64) {
+  private requisicaoParaAPI(imageRequest) {
     const apiKey = 'AIzaSyBLNjgYqCgkbLFNo5vsr-2m9vb7Yp6zz8g';
     const url = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
     const request = {
       requests: [
         {
-          image: {
-            content: imagemBase64
-          },
+          image: imageRequest,
           features: [
             {
               type: 'LABEL_DETECTION',
@@ -87,28 +106,33 @@ export class HomePage {
       )
       .subscribe(
         (result: AnnotateImageResponse) => {
-          let hasStreetSign = false;
+          this.hasStreetSign = false;
+          const streetSignDescriptions = [
+            'Street sign', 'Traffic sign', 'Metropolitan area'
+          ];
 
           for (const labelAnnotation of result.responses[0].labelAnnotations) {
-            if (labelAnnotation.description === 'Street sign') {
-              hasStreetSign = true;
+            if (streetSignDescriptions.some(d => labelAnnotation.description)) {
+              this.hasStreetSign = true;
+              this.address =
+                result.responses[0]
+                  .fullTextAnnotation
+                  .text
+                  .replace(/\n/g, ' ');
               break;
             }
-          }
-
-          if (!hasStreetSign) {
-            alert(
-              'Isso não parece uma placa de rua!\n' +
-              'Por favor, tente de novo'
-            );
-          } else {
-            alert(
-              'A rua foi identificada\n' +
-              'Iremos mostrá-la no mapa!'
-            );
           }
         },
         (error: HttpErrorResponse) => { alert(error.error.error.message); }
       );
+  }
+
+  localizarEndereco(){
+    this.router.navigate(['/location', '']);
+  }
+
+  limpar() {
+    this.imagem = null;
+    this.hasStreetSign = null;
   }
 }
